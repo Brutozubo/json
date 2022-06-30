fc1937b4c77e08bad4bd402d45c41022e91a5e97
Sergey Nikolaev <107795096+Brutozubo@users.noreply.github.com>
2022-06-30 15:24:21 +0500

    Update json_2.py

--- json_2.py
+++ json_2.py
@@ -1,36 +1,35 @@
 
+logins = []
+passwords = []
+data_user = logins + passwords
+
 import json
 
-login = []
-passwd = []
-
 def register():
-    login.append(input("Выберите ваш login: "))
-    passwd.append(input("Выберите ваш пароль: "))
 
-def create_json():
-    data_user = [{"login": login, "пароль": passwd}]
+    logins.append(input("Создайте login: "))
+    passwords.append(input("Создайте пароль: "))
+
     with open('data.json', 'w') as f:
         json.dump(data_user, f)
 
-def user_login():
-    login = input("Введите ваш login: ")
-    
-    with open('data.json') as f:
+def login():
+
+    login = input("Введите существующий login: ")
+    password = input("Введите существующий пароль: ")
+
+    with open('data.json', 'r') as f:
         data_user = json.load(f)
 
-    if login in data_user:
-        print(f"Да, {login} существует")
-        return True
+    if login in logins and password in passwords:
+        print("Добро пожаловать!")
     else:
-        print(f"Нет такого {login}")
-        return False
-    
-def login_function(login, passwd):
-    with open ('data.json', 'r') as f:
-        data_user = json.load(f)
+        print("Некорректные данные!")
 
-    if login and passwd in data_user:
-         print('Успешный вход!')
-     else:
-         print('Неверный логин или пароль')
+
+while True:
+    account_ans = input("Выберите пункт:  a)Зарегистрироваться     b)Войти в систему")
+    if account_ans == "a":
+        register()
+    if account_ans == "b":
+        login()
